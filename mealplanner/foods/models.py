520e4c0a7fca7848c81f56ca64daf6db755c7461
Nathan McCorkle <nathan.mccorkle@relativity.com>
2023-01-03 16:46:56 -0600

    updates

--- mealplanner/foods/models.py
+++ mealplanner/foods/models.py
@@ -1,13 +1,21 @@
 from django.db import models
+from django.utils import timezone
 
 
 class FoodType(models.Model):
     name = models.CharField(max_length=200)
+    created_datetime = models.DateTimeField(auto_now_add=True)
+    updated_datetime = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Food(models.Model):
     name = models.CharField(max_length=200)
-    foodtype = models.ForeignKey(FoodType, on_delete=models.CASCADE, null=True)
+    food_type = models.ForeignKey(FoodType, on_delete=models.CASCADE, null=True)
+    created_datetime = models.DateTimeField(auto_now_add=True)
+    updated_datetime = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Setup
+```
+python.exe -m venv .venv
+. .\.venv\Scripts\activate
+pip install -r requirements.txt
+```
+
+# Generate new requirements.txt file
+`pip freeze > .\requirements.txt`
