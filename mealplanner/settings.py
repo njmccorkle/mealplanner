2882551cb88b1fd5d236b878bc8b06c7eec55373
Nathan McCorkle <njmccorkle@gmail.com>
2023-10-30 22:41:18 -0500

    switch to sqlite

--- mealplanner/settings.py
+++ mealplanner/settings.py
@@ -79,22 +79,22 @@ WSGI_APPLICATION = "mealplanner.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
-
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "mealplanner",
-        "USER": "mealplanner",
-        "HOST": "docker.mccorkle.co",
-        "PORT": "6033",  # 3306 is correct port when mysql isn't behind traefik
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.mysql",
+#         "NAME": "mealplanner",
+#         "USER": "mealplanner",
+#         "HOST": "docker.mccorkle.co",
+#         "PORT": "6033",  # 3306 is correct port when mysql isn't behind traefik
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
